bec9f181d44a49e83fc36c6120cc1c4a5e6fa994
ignorantcaomao <caomaodjs@163.com>
2024-09-17 16:20:48 +0800

    添加项目配置文件

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    docs_url=f"{settings.API_V1_STR}/docs"
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": settings}
