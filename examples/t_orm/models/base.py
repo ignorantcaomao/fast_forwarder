01aa2f72f88a6934a6beee331cd15a9b679f45d0
无知草帽 <caomaodjs@163.com>
2024-11-20 15:00:25 +0800

    删除多余的文件

--- examples/t_orm/models/base.py
+++ examples/t_orm/models/base.py
@@ -2,16 +2,26 @@ from tortoise import fields
 from tortoise.models import Model
 
 
-class User(Model):
+class TimestampMixin(Model):
+    created_at = fields.DatetimeField(auto_now_add=True, description="创建时间")
+    modified_at = fields.DatetimeField(auto_now=True, description="修改时间")
+
+    class Meta:
+        abstract = True
+
+
+class User(TimestampMixin):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=100)
     password = fields.CharField(max_length=255, description="用户名密码")
     email = fields.CharField(max_length=255, description="用户邮箱")
+    is_active = fields.BooleanField(default=True, description="是否激活")
 
     role = fields.ForeignKeyField('models.Role', related_name='users')
 
     class Meta:
         table = 'user'
+        table_description = '用户表'
 
 
 class Role(Model):
